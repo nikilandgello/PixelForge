3e6c2772e470b49f675ebfd6cb20e7c5c845ae85
nikilandgello <nikoscha22012004@gmail.com>
2024-10-31 18:21:56 +0200

    update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,12 +3,21 @@
     openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-menu-close]'),
     modal: document.querySelector('[data-menu]'),
+    modalMenu: document.querySelector('.mobile-menu-list'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+  refs.modalMenu.addEventListener('click', (event) => {
+    let tagName = event.target.tagName.toLowerCase();
+
+    if (tagName === 'a') {
+      toggleModal();
+    };
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
-  }
+  };
 })();
